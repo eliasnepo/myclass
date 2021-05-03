8416fa92af1f5eeb0f16baedb4f4af9c88b186a9
João Pedro <joaopedrosf1903@gmail.com>
2021-05-03 00:24:50 -0300

    Conversão de Typescript para Javascript

--- front-web/src/components/Navbar/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-export default function Navbar() {
-    return(
-        <nav className="main-navbar">
-            <h2 className="title-nav">MyClass</h2>
-        </nav>
-    );
-}
