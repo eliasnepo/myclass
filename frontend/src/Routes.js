0da6ed64e48a2c1e117ab43d5970f5455bb75e62
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-05-06 14:01:33 -0300

    Add auth page

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,10 +1,11 @@
 import { Route, Router, Switch } from "react-router";
 import Navbar from "./core/components/Navbar/Navbar";
 import history from "./core/utils/history";
-import Login from "./Login";
 import Course from "./pages/Course";
 import GetDeliveries from "./pages/GetDeliveriesAndRevision";
 import Home from "./pages/Home";
+import Login from './pages/Auth/Login/index'
+import Register from './pages/Auth/Register/index'
 import InsertLesson from "./pages/InsertLesson";
 
 const Routes = () => {
@@ -29,6 +30,9 @@ const Routes = () => {
                 </Route> */}
                 <Route path="/login">
                     <Login/>
+                </Route>
+                <Route path="/register">
+                    <Register/>
                 </Route>
             </Switch>
         </Router>
